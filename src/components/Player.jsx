c4668f1a8bce796b759a67dd68a98173d9069755
Luis De La Torre A <dev.delatorre.l@gmail.com>
2024-04-14 22:29:47 -0500

    added state and two way binding to set the name of the player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  
+  const [name, setName] = useState("");
+  
+  let newName = "";
+  
+  function ClickHandler(){
+    setName(newName);
+  }
+
+  function ChangeName(event){
+    newName = event.target.value; 
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {(name) ? name : "unknown entity" }</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={(e) => ChangeName(e)}/>
+        <button onClick={ClickHandler}>Set Name</button>
       </p>
     </section>
   );
