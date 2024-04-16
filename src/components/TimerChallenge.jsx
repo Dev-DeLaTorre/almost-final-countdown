105a4141151d2a7f7138b50c7fabfe62dd6ff307
Luis De La Torre A <dev.delatorre.l@gmail.com>
2024-04-15 23:06:54 -0500

    added states to manage the status of the button, timer and validations for the jsx

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 export default function TimerChallenge({title, targetTime}){
+    
+    const [startTimer, setStartTimer] = useState(false);
+    const [expiredTimer, setExpiredTimer] = useState(false);
+
+    function StartHandler(){
+        setStartTimer(true);
+        setTimeout(()=>{ setExpiredTimer(true) }, targetTime * 1000);
+    }
+    
+    function StopHandler(){
+
+    }
+
     return(
         <section className="challenge">
             <h2>{title}</h2>
+            {expiredTimer && <p>You Lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={StartHandler}>
+                    {startTimer ? "Stop" : "Start"} Challenge
                 </button>
             </p>
-            <p className="">
-                Time is running... / Timer inactive
+            <p className={startTimer ? "active" : undefined}>
+                {startTimer ? "Time is running..." : "Timer inactive"}
             </p>
         </section>
     )
